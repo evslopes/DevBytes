18d00d66b5ccc9378987b24d9bec5262e48b9af6
Elvis Lopes <elvis.lopes@al.infnet.edu.br>
2021-04-01 18:55:43 -0300

    Recuperarando dados do banco de dados

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,7 +16,11 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
 import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -34,6 +38,10 @@ class VideosRepository(private val database: VideosDatabase) {
      * function is now safe to call from any thread including the Main thread.
      *
      */
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
     suspend fun refreshVideos() {
         withContext(Dispatchers.IO) {
             val playlist = DevByteNetwork.devbytes.getPlaylist()
